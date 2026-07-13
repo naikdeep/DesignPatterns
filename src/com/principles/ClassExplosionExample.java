7699ae2cdedee87b19268831586fed2412807766
naikdeep <148378222+naikdeep@users.noreply.github.com>
2026-07-14 00:38:58 +0530

    Adding class explosion example

--- src/com/principles/ClassExplosionExample.java
+++ src/com/principles/ClassExplosionExample.java
@@ -1,5 +1,25 @@
 package com.principles;
 
 public class ClassExplosionExample {
+    
+	/*Imagine you are building a pizza 
+	ordering system and want to account for all 
+	possible pizza types. You might initially
+	 
+	create:Pizza (Base class)
+	
+	
+	If you use inheritance to create specific combinations,
+	a class explosion occurs:
+		
+		MargheritaPizza
+		MargheritaExtraCheese
+		PizzaPepperoniPizza
+		PepperoniExtraCheese
+		PizzaMargheritaGluten
+		FreePizzaPepperoniGluten
+		FreeExtraCheesePizza
 
+*/
+	
 }
